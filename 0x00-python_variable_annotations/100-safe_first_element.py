b30da248fd993ddb79a80c652b3e459a9e166333
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-01 17:53:17 +0300

    Complex use mypy

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""safe_first_element function def"""
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Return first element of list if exists, otherwise None"""
+    if lst:
+        return lst[0]
+    else:
+        return None
