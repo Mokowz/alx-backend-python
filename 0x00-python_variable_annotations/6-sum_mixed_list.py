307afbf36a8f30a6a996edd05b813f86317c5a10
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-01 17:32:15 +0300

    Fix types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """sum_mixed_list function def"""
-from typing import Union, Optional
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum_mixed_list function"""
     return sum(mxd_lst)
