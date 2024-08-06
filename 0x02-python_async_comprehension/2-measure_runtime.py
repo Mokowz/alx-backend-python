18a3a1bacda728c313eb304ab4b37ea2031edcf6
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-06 13:41:40 +0300

    Asyncio gather and run 3 processes in parallel

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Measure runtime for 4 parallel comprehensions"""
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure runtime for 4 parallel comprehensions"""
+    start = perf_counter()
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+    end = perf_counter()
+    return (end - start)
