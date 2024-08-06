0357bec34ca1964f5fa6a757a6e5b0552dcac308
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-06 13:36:44 +0300

    Utilize async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Loop 10 times as a generator"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Loop 10 times"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Loop 10 times as an async comprehension"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """"Loop 10 times and append to a list"""
+    return [i async for i in async_generator()]
