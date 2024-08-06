95ecf8992636848459b8acf99083d6a96b4a83e6
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-06 12:59:23 +0300

    Fix styles

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,5 +8,5 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Waits for a random delay n times"""
-    tasks = [asyncio.create_task(task_wait_random(max_delay)) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
