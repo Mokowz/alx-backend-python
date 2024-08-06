96d0cc1c9da7a3e2436149bf31759a34a11870c3
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-06 12:49:34 +0300

    Create a task using regular function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""Concurrent coroutines"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+  """Waits for a random delay n times"""
+  return [await wait_random(max_delay) for _ in range(n)]
