ad41d0438cf463f3b68aaaf366302b85cb21692d
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-26 14:05:43 +0300

    Mock and test a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test Client Module"""
 import unittest
 from unittest import mock
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 from utils import get_json
@@ -21,6 +21,17 @@ class TestGithubOrgClient(unittest.TestCase):
         ex_response = ex.org
         self.assertEqual(ex_response, mock_org.return_value)
         mock_org.assert_called_once()
+
+    def test_public_repos_url(self):
+        """Test Public Repo URL"""
+        with patch.object(GithubOrgClient, 'org',
+                          new_callable=PropertyMock) as MockClass:
+            MockClass.return_value = {"repos_url": 100}
+            ex = GithubOrgClient('Mimi')
+            ex_response = ex._public_repos_url
+            self.assertEqual(ex_response,
+                             MockClass.return_value.get("repos_url"))
+            MockClass.assert_called_once()
 
 
 if __name__ == '__main__':
