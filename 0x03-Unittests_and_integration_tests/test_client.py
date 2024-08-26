be90d8c820e9e03e07ce7d518a02bc6aeed31260
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-26 14:18:05 +0300

    Implement different parameterizations

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,6 +37,20 @@ class TestGithubOrgClient(unittest.TestCase):
                              MockClass.return_value.get("repos_url"))
             MockClass.assert_called_once()
 
+    @patch('client.get_json', return_value=[
+        {"name": "Hello"}, {"name": "Haha"}])
+    def test_public_repos(self, mock_get):
+        """Test Public Repos"""
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock,) as moc:
+            # moc.return_value = [{"name": "Hello"}, {"name": "Haha"}]
+            git1 = GithubOrgClient('mimi')
+            test_response = git1.public_repos()
+            for i in range(2):
+                self.assertIn(mock_get.return_value[i]["name"], test_response)
+            mock_get.assert_called_once()
+            moc.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
