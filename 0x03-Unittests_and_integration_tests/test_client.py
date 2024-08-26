8f4845c483697fe8652e9cbfcfbc2412007cba27
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-26 13:42:00 +0300

    Parameterized and patched test org function

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""Test Client Module"""
+import unittest
+from unittest import mock
+from unittest.mock import Mock, patch
+from parameterized import parameterized
+from client import GithubOrgClient
+from utils import get_json
+
+
+class TestGithubOrgClient(unittest.TestCase):
+  """Test Github Org Client class"""
+  @parameterized.expand([
+    ("google"),
+    ("abc"),
+  ])
+  @patch('client.get_json', return_value={"payload": True})
+  def test_org(self, org, mock_org):
+    "Test org function"
+    ex = GithubOrgClient(org)
+    response = ex.org
+    self.assertEqual(response, mock_org.return_value)
+    mock_org.assert_called_once()
+
+
+if __name__ == '__main__':
+  unittest.main()
