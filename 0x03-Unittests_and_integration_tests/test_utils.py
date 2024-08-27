ddef08aae467e7c4a69943f40e4e783ba41a1a2e
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-08-27 10:46:53 +0300

    Fix the get json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,16 +35,20 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    @patch("requests.get")
-    def test_get_json(self, mock_get, test_url, test_payload):
-        """Test get json func"""
-        mock_response = Mock()
-        mock_response.return_value = test_payload
-        mock_get.return_value = mock_response
-        result = get_json(test_url)
+    def test_get_json(self, url, payload):
+        """
+        Test get json function
+        """
+        class Mocked(Mock):
+            """Mocked class"""
 
-        self.assertEqual(result, test_payload)
-        mock_get.assert_called_once()
+            def json(self):
+                """json function"""
+                return payload
+
+        with patch("requests.get") as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), payload)
 
 
 class TestMemoize(unittest.TestCase):
